0124a6ef3afa6c6f69808853be2bec3bb1b5ce46
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-10-24 19:52:12 +1000

    prompt-sync

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-let {names} = require('./messageList')
+const promptSync = require('prompt-sync');
 
-function saySomething(somethingToSay){
-    console.log(somethingToSay)
-}
+const promptInstance = promptSync({
+    sigint: true //allows CTRL+C to exit the app
+})
 
-saySomething(names)
-saySomething("Welcome to Node.js!")
-saySomething("Pretty cool, right?")
+let userEnteredValid = false;
 
-// for (const name of names) {
-//     saySomething(name)
+do {
+    let userInput = promptInstance("How old are you?");
+    let userInputAsNumber = Number(userInput)
+
+    if (userInputAsNumber >= 18 ){
+        console.log("You are good to vote");
+        userEnteredValid = true;
+    }else{
+        console.log("Not a valid age for voting");
+    }
+} while (!userEnteredValid)
+
+// let {names} = require('./messageList')
+
+// function saySomething(somethingToSay){
+//     console.log(somethingToSay)
 // }
 
-names.forEach(item => {
-    saySomething(item);
-});
+// saySomething(names)
+// saySomething("Welcome to Node.js!")
+// saySomething("Pretty cool, right?")
+
+// // for (const name of names) {
+// //     saySomething(name)
+// // }
+
+// names.forEach(item => {
+//     saySomething(item);
+// });
